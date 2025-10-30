91b62c4695c866a2c353a4f1e667702f67954c2b
馍馍喂我 <3108223499@qq.com>
2025-10-30 23:30:07 +0800

    2025.10.30 文章h1标题随机云纹实现方式改为svg嵌入

--- .vitepress/theme/components/CustomDOC.js
+++ .vitepress/theme/components/CustomDOC.js
@@ -1,14 +1,32 @@
+import cloud1 from '../../assests/cloud/clouding1.svg?raw'
+import cloud2 from '../../assests/cloud/clouding2.svg?raw'
+import cloud3 from '../../assests/cloud/clouding3.svg?raw'
+import cloud4 from '../../assests/cloud/clouding4.svg?raw'
+import cloud5 from '../../assests/cloud/clouding5.svg?raw'
+import cloud6 from '../../assests/cloud/clouding6.svg?raw'
+import cloud7 from '../../assests/cloud/clouding7.svg?raw'
+import cloud8 from '../../assests/cloud/clouding8.svg?raw'
+import cloud9 from '../../assests/cloud/clouding9.svg?raw'
+import cloud10 from '../../assests/cloud/clouding10.svg?raw'
+import cloud11 from '../../assests/cloud/clouding11.svg?raw'
+import cloud12 from '../../assests/cloud/clouding12.svg?raw'
+import cloud13 from '../../assests/cloud/clouding13.svg?raw'
+import cloud14 from '../../assests/cloud/clouding14.svg?raw'
+
+const clouds = [cloud1, cloud2, cloud3, cloud4, cloud5, cloud6, cloud7, cloud8, cloud9, cloud10, cloud11, cloud12, cloud13, cloud14]
+
+
 // 给每篇文章的标题处添加图片
 export const patchH1 = ()=>{
     const h1 = document.querySelector('.vp-doc h1')
-    console.log(h1)
-    if (h1 && !h1.querySelector('img')) {
-        const img = document.createElement('img')
-        // 1到14的随机整数
-        img.src = `/blog/cloud/clouding${Math.floor(Math.random() * 13 + 1)}.svg`
-        img.style.height = '1em'
-        img.style.marginLeft = '0.5em'
-        h1.appendChild(img)
+    if (h1 && !h1.querySelector('span')) {
+        const span = document.createElement('span')
+        const index = Math.floor(Math.random() * clouds.length)
+        span.innerHTML = clouds[index]
+        const svg = span.firstElementChild
+        svg.style.height = '1em'
+        svg.style.marginLeft = '0.5em'
+        h1.appendChild(span)
     }
 }
 
